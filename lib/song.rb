1ade04e6cf3322395134dce5ba90deb52958adf8
Mokhtar Ali <joemokhtarali@gmail.com>
2020-01-12 21:12:48 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,11 @@
+require 'pry'
 class Song 
   
-  @@count = 0
+  @@count = 0 
   @@artists = []
   @@genres = []
   @@artist_count = {}
-  @@genre_count = {} 
+  @@agenre_count = {}
   
   attr_accessor :name, :artist, :genre, :count, :artists, :genres, :artist_coun, :genre_count
   
@@ -12,14 +13,49 @@ class Song
     @name = name 
     @artist = artist
     @genre = genre
-    @@count += 1 
+    @@count += 1  
     @@artists << @artist
     @@genres << @genre
     
   end 
+
+  def self.artists
+    @@artists.uniq
+  end 
   
-  def artists_count
-    @artists.each do |artist|
+  def self.genres
+    @@genres.uniq
+  end 
+  
+  def self.count 
+    return @@count 
+  end 
+  
+  def self.genre_count 
+        @@genres.each do |genre| 
+           if @@genre_count[genre] 
+              @@genre_count[genre] +=1
+           else @@genre_count[genre] =1
+            end
+        end
+        @@genre_count  
+    end
+  
+  # def self.genre_count
+    
+  #   @@genres.each do |genre|
+  #     # binding.pry 
+  #     if @@genre_count[genre]
+  #       @@agenre_count[genre] += 1
+  #     else 
+  #       @@genre_count[genre] = 1
+  #     end 
+  #   end 
+  #     @@genre_count
+  #   end 
+  
+  def self.artist_count
+    @@artists.each do |artist|
       if @@artist_count[artist]
         @@artist_count[artist] += 1
       else 
@@ -28,24 +64,5 @@ class Song
     end 
       @@artist_count
 end 
-  
-  def genre_count
-    @@genres.each do |genre|
-      if @@genre_count[genre]
-        @@agenre_count[genre] += 1
-      else 
-        @@genre_count[genre] = 1
-      end 
-    end 
-      @@genre_count
-    end 
-  
-  def artists
-    @@artists.uniq
-  end 
-  
-  def genres
-    @@genres.uniq
-  end 
   
   end 
